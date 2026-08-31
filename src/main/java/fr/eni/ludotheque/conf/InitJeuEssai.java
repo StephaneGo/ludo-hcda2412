93ba0b519e917c16bf961f467916943ee6cabffd
Stephane <sgobin@campus-eni.fr>
2026-08-31 16:57:37 +0200

    mise en place jeu essai

--- src/main/java/fr/eni/ludotheque/conf/InitJeuEssai.java
+++ src/main/java/fr/eni/ludotheque/conf/InitJeuEssai.java
@@ -1,8 +1,12 @@
 package fr.eni.ludotheque.conf;
 
 
+import fr.eni.ludotheque.bo.Exemplaire;
 import fr.eni.ludotheque.bo.Genre;
+import fr.eni.ludotheque.bo.Jeu;
+import fr.eni.ludotheque.dal.ExemplaireRepository;
 import fr.eni.ludotheque.dal.GenreRepository;
+import fr.eni.ludotheque.dal.JeuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +18,19 @@ public class InitJeuEssai {
     @Autowired
     private GenreRepository genreRepository;
 
+    @Autowired
+    private JeuRepository jeuRepository;
+
+    @Autowired
+    private ExemplaireRepository exemplaireRepository;
+
     @Bean
     public CommandLineRunner jeuEssai(){
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
                 //Jeu essai
+                //Création des genres
                 Genre cartes = Genre.builder()
                         .noGenre(1L)
                         .libelle("cartes")
@@ -31,8 +42,46 @@ public class InitJeuEssai {
                         .build();
 
                 genreRepository.deleteAll();
-                genreRepository.save(cartes);
-                genreRepository.save(strategie);
+                Genre cartesSaved = genreRepository.save(cartes);
+                Genre strategieSaved = genreRepository.save(strategie);
+
+                //Création des jeux
+                jeuRepository.deleteAll();
+
+                Jeu foretMixte = Jeu.builder()
+                        .titre("Foret mixte")
+                        .description("description du jeu foret mixte")
+                        .duree(30)
+                        .ageMin(12)
+                        .tarifJour(4.5f)
+                        .reference("ref-foret-mixte")
+                        .build();
+                foretMixte.addGenre(cartesSaved);
+                foretMixte.addGenre(strategieSaved);
+                Jeu foretMixteSaved = jeuRepository.save(foretMixte);
+
+                Jeu skyjo = Jeu.builder()
+                        .titre("Skyjo")
+                        .description("description du jeu skyjo")
+                        .duree(20)
+                        .ageMin(7)
+                        .tarifJour(4f)
+                        .reference("ref-skyjo")
+                        .build();
+                skyjo.addGenre(cartesSaved);
+                Jeu skyJoSaved = jeuRepository.save(skyjo);
+
+                //Création des exemplaires
+                exemplaireRepository.deleteAll();
+
+                Exemplaire exemplaireFM = Exemplaire.builder()
+                        .codebarre("101")
+                        .louable(true)
+                        .jeu(foretMixteSaved)
+                        .build();
+
+                //Act : appel de la méthode à tester
+                Exemplaire savedFM = exemplaireRepository.save(exemplaireFM);
 
             }
         };
